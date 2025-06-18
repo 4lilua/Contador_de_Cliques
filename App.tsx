72fa6d4a999c0fe5fcfb03c3d012bd2416340068
4lilua <alinesm0503@gmail.com>
2025-06-18 10:39:41 -0300

    resolver codigo

--- App.tsx
+++ App.tsx
@@ -1,9 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, Alert } from 'react-native';
+import { View, Text, Button, StyleSheet, Alert, ImageSourcePropType, Pressable, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { styles } from './BotaoInterativoStyle';
+
+const emotes: ImageSourcePropType[] = [
+  require('./assets/amor.avif'),
+  require('./assets/chorando.avif'),
+  require('./assets/corada.avif'),
+  require('./assets/descolada.avif'),
+  require('./assets/do.avif'),
+  require('./assets/doente.avif'),
+  require('./assets/dormindo.avif'),
+  require('./assets/empolgada.avif'),
+  require('./assets/feliz.avif'),
+  require('./assets/fome.avif'),
+  require('./assets/mal.avif'),
+  require('./assets/medo.avif'),
+  require('./assets/morta.avif'),
+  require('./assets/normal.avif'),
+  require('./assets/paz.avif'),
+  require('./assets/pensativa.avif'),
+  require('./assets/piscadela.avif'),
+  require('./assets/raivosa.avif'),
+  require('./assets/rindo.avif'),
+  require('./assets/timida.avif'),
+  require('./assets/triste.avif'),
+  require('./assets/vergonha.avif'),
+]
 
 export default function App() {
+  const [img, setImg] = useState(emotes);
   const [contador, setContador] = useState(0);
+
+  const [qtd, setQtd] = useState(0);
+  const [tempo, setTempo] = useState(0);
+  const [media, setMedia] = useState(0);
+
+  let Img = imagens[qtd];
 
   const incrementar = () => {
     setContador(contador + 1);
@@ -12,6 +45,59 @@ export default function App() {
   const decrementar = () => {
     setContador(contador - 1);
   };
+
+  function mostrar_imagem() {
+    let i = 0;
+    for(i = 0; i <= qtd; i++){
+      let img = imagens[i];
+    }
+
+  }
+
+  function mostrar_imagem() {
+    let qtdtemp = qtd + 1
+    setQtd(qtdtemp)
+  }
+
+function pin() {
+    setImg(galinhaVermelha)
+    console.log(qtd)
+    if(qtd == 0){
+        setTempo(0)
+    }
+}
+
+
+function pout(){
+    setImg(galinha)
+}
+
+function plongo(){
+    mediaTempo()
+    Alert.alert("Média de cliques por segundo", ''+media)
+}
+
+useEffect(() => {
+    const timer = setInterval(() => {
+        setTempo((prevCount) => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [tempo]);
+
+function mediaTempo() {
+    console.log(qtd)
+    console.log(tempo)
+    setMedia(qtd / tempo)
+  }
+
+function zerar() {
+    setMedia(0)
+    setQtd(0)
+    setTempo(0)    
+}
+
+
 
   // Efeito para detectar quando contador chega a zero
   useEffect(() => {
@@ -30,6 +116,13 @@ export default function App() {
 
     >
       <View style={styles.container}>
+      <Pressable onPress={(aumentar)}
+                              onPressIn={pin}
+                              onPressOut={pout}
+                              onLongPress={plongo}
+            >
+                <Image style={styles} source={img}></Image>
+            </Pressable>
         <Text style={styles.texto}>Contador: {contador}</Text>
         <View style={styles.botoes}>
           <Button title="Incrementar" onPress={incrementar} />
@@ -57,3 +150,87 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
 });
+
+
+
+
+
+
+
+const galinha = require('../../../assets/GalinhaNormal.png');
+const galinhaVermelha = require('../../../assets/GalinhaApanhando.png');
+
+
+export function BotaoGalinha() {
+    const [qtd, setQtd] = useState(0);
+    const [img, setImg] = useState(galinha);
+    const [tempo, setTempo] = useState(0);
+    const [media, setMedia] = useState(0);
+
+
+    function aumentar() {
+        let qtdtemp = qtd + 1
+        setQtd(qtdtemp)
+    }
+    
+    function pin() {
+        setImg(galinhaVermelha)
+        console.log(qtd)
+        if(qtd == 0){
+            setTempo(0)
+        }
+    }
+    
+
+    function pout(){
+        setImg(galinha)
+    }
+
+    function plongo(){
+        mediaTempo()
+        Alert.alert("Média de cliques por segundo", ''+media)
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTempo((prevCount) => prevCount + 1);
+        }, 1000);
+    
+        return () => clearInterval(timer);
+      }, [tempo]);
+
+    function mediaTempo() {
+        console.log(qtd)
+        console.log(tempo)
+        setMedia(qtd / tempo)
+      }
+    
+    function zerar() {
+        setMedia(0)
+        setQtd(0)
+        setTempo(0)    
+    }
+
+    return (
+        <View style={styles.container}>
+            <Pressable onPress={(aumentar)}
+                              onPressIn={pin}
+                              onPressOut={pout}
+                              onLongPress={plongo}
+            >
+                <Image style={styles.galinhaimagem} source={img}></Image>
+            </Pressable>
+            
+            <View style={styles.viewStats}>
+                <Text  style={styles.stats}>Total de Cliques: {qtd}</Text>
+                <Text  style={styles.stats}>Tempo Total: {tempo}</Text>
+                <Text  style={styles.stats}>Velocidade de Cliques: {media}</Text>
+            </View>
+
+            <Pressable style={styles.zerar} onPress={(zerar)}>
+                <Text style={styles.texxtozerar}>ZERAR</Text>
+            </Pressable>
+        </View>
+    
+  );
+}
